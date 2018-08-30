e97975aa9ed5f5178cee7f0c3377d2de2961bac9
BhargaviDudharejiya <dudharejiab@gmail.com>
2018-08-29 23:45:42 -0400

    To Do app Complete

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -16,14 +16,15 @@ export class TodoService extends Init{
     var todos = JSON.parse(localStorage.getItem('todos'));
     return todos;
   }
-
+//-----------------------------Add new item to Todo list
   addTodo(newTodo){
     var todos = JSON.parse(localStorage.getItem('todos'));
-    //Add New Todo
+    //Add New Todo to the original object
     todos.push(newTodo);
-    //set new todo
+    //set new todo to localstorage
     localStorage.setItem('todos',JSON.stringify(todos));
   }
+//---------------------------Delete selected item from todo list
   deleteTodo(todoText){
     var todos = JSON.parse(localStorage.getItem('todos'));
     for (let i = 0; i < todos.length; i++) {
@@ -31,6 +32,19 @@ export class TodoService extends Init{
         todos.splice(i, 1);
       }
     }
+    //Set new Todos in localstorage
+    localStorage.setItem('todos',JSON.stringify(todos));
+  }
+//---------------------------UpdateTodo list
+  updateTodo(oldText,newText){
+    var todos = JSON.parse(localStorage.getItem('todos'));
+
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].text == oldText) {
+        todos[i].text = newText;
+      }
+    }
+    //Set New Todos in localstorage
     localStorage.setItem('todos',JSON.stringify(todos));
   }
 
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { TodoService } from '../todo.service';
-import * as $ from 'jquery';
+import { TodoService } from "../todo.service";
+import * as $ from "jquery";
 @Component({
   selector: "app-todos",
   templateUrl: "./todos.component.html",
@@ -9,24 +9,24 @@ import * as $ from 'jquery';
 export class TodosComponent implements OnInit {
   todos;
   text;
-  appState = 'default'; 
+  appState = "default";
   oldText;
   constructor(private _todoService: TodoService) {}
 
   ngOnInit() {
-      this.todos = this._todoService.getTodos();
+    this.todos = this._todoService.getTodos();
   }
-
+//-----------------Add new item to todo list
   addTodo() {
     var newTodo = {
-      text: this.text 
-    }
+      text: this.text
+    };
     console.log(this.text);
     console.log(newTodo);
     this.todos.push(newTodo);
     this._todoService.addTodo(newTodo);
   }
-
+//-----------------delete selected from todo list
   deleteTodo(value) {
     for (let i = 0; i < this.todos.length; i++) {
       if (this.todos[i].text == value) {
@@ -35,22 +35,25 @@ export class TodosComponent implements OnInit {
     }
     this._todoService.deleteTodo(value);
   }
-
-  editTodo(value){
-    this.appState = 'edit';
+//----------------edit the selected item
+  editTodo(value) {
+    this.appState = "edit";
     this.oldText = value.text;
-      this.text = value.text;
+    this.text = value.text;
     console.log(this.oldText);
     console.log(value);
     console.log(this.text);
     console.log(this.appState);
   }
-
-  UpdateTodo(){
+//---------------update the selected item
+  updateTodo() {
     console.log(this.text);
     for (let i = 0; i < this.todos.length; i++) {
       if (this.todos[i].text == this.oldText) {
         this.todos[i].text = this.text;
+      }
     }
+
+    this._todoService.updateTodo(this.oldText,this.text);
   }
 }
